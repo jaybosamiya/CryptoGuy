9db91ccd06265cc278dffc1a2aa7e746b3922780
Jay Bosamiya <jaybosamiya@gmail.com>
2015-03-22 03:21:40 +0530

    Add trivial decryptions

--- src/cryptoguy.cpp
+++ src/cryptoguy.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 namespace decrypter {
 	string encrypted_string, key;
+	void init();
 	void run_all();
 };
 
@@ -19,6 +20,7 @@ int main(int argc, char ** argv) {
 		decrypter::key = string(argv[2]);
 	}
 
+	decrypter::init();
 	decrypter::run_all();
 
 	return 0;
@@ -26,16 +28,30 @@ int main(int argc, char ** argv) {
 
 namespace decrypter {
 
-	typedef string(*DecryptionFunction)(string); // should return name of encryption algo on empty input
-	typedef string(*KeyedDecryptionFunction)(string,string); // should return name of encryption algo on empty input
+	typedef string(*DecryptionFunction)(string e); // should return name of encryption algo on empty input
+	typedef string(*KeyedDecryptionFunction)(string e, string k); // should return name of encryption algo on empty input
 
 	vector<DecryptionFunction> dfv;
 	vector<KeyedDecryptionFunction> kdfv;
 
-	void init() {
-		// dfv.push_back(/*function*/);
-		// kdfv.push_back(/*function*/);
+	string trivial_decryption (string e) {
+		if ( e.empty() )
+			return "Trivial";
+		return e;
 	}
+
+	string trivial_keyed_decryption (string e, string k) {
+		if ( e.empty() )
+			return "Trivial Keyed";
+		return e+k;
+	}
+
+	void init() {
+		dfv.push_back(trivial_decryption);
+		kdfv.push_back(trivial_keyed_decryption);
+	}
+
+	void output_all(vector<string> decryption_algo, vector<string> decrypted_strings);
 
 	void run_all() {
 		vector<string> decrypted_strings;
@@ -45,11 +61,19 @@ namespace decrypter {
 				decryption_algo.push_back((*it)(string()));
 				decrypted_strings.push_back((*it)(encrypted_string));
 			}
+			output_all(decryption_algo,decrypted_strings);
 		} else {
 			for ( vector<KeyedDecryptionFunction>::iterator it = kdfv.begin() ; it != kdfv.end() ; it++ ) {
 				decryption_algo.push_back((*it)(string(),string()));
 				decrypted_strings.push_back((*it)(encrypted_string,key));
 			}
+			output_all(decryption_algo,decrypted_strings);
+		}
+	}
+
+	void output_all(vector<string> decryption_algo, vector<string> decrypted_strings) {
+		for ( int i = 0 ; i < (int)decrypted_strings.size() ; i++ ) {
+			cout << decryption_algo[i] << ":\t" << decrypted_strings[i] << endl;
 		}
 	}
 
