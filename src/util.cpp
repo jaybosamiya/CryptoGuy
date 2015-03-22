8ebb84b7c7ff8193e2436bedd8bf13126b9ddd58
Jay Bosamiya <jaybosamiya@gmail.com>
2015-03-22 16:18:40 +0530

    Add modulo utilities

--- src/util.cpp
+++ src/util.cpp
@@ -13,3 +13,25 @@ vector<int> read_ints(string s) {
 	}
 	return ret;
 }
+
+int modPow(int a, int b, int MOD) {
+	int x = 1, y = a;
+	while(b > 0) {
+		if(b%2 == 1) {
+			x=(x*y);
+			if(x>MOD) x%=MOD;
+		}
+		y = (y*y);
+		if(y>MOD) y%=MOD;
+			b /= 2;
+	}
+	return x;
+}
+ 
+int modInverse(int a, int m) {
+    a %= m;
+    for(int x = 1; x < m; x++) {
+        if((a*x) % m == 1) return x;
+    }
+    return -1;
+}
--- src/util.h
+++ src/util.h
@@ -5,5 +5,7 @@
 #include <string>
 
 std::vector<int> read_ints(std::string s);
+int modPow(int a, int b, int MOD);
+int modInverse(int a, int m);
 
 #endif
